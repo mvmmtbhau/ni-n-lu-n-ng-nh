fae3a3cc1881f58551eb788de37098fff595c845
mvmmtbhau <nguyen1hau2211@gmail.com>
2023-01-31 12:42:17 +0700

    Cai dat du an

---
+++ src/server.js
@@ -0,0 +1,7 @@
+const app = required('./app');
+const config = require('./app/config');
+
+const PORT = config.app.port;
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+})
